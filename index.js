24c30ac06288ccb183ceb061e7081752002dbc5b
surenderthakran <surender.thakran89@gmail.com>
2016-01-10 02:40:07 +0530

    good logging plugin added

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 
 const Hapi = require("hapi");
 const Inert = require("inert");
+const Good = require("good");
+const GoodConsole = require("good-console");
 
 const server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({
+    host: "0.0.0.0",                        // @TODO: decide between "localhost" and "0.0.0.0" for docker containers
+    port: 3000
+});
 
 server.register(Inert, (err) => {
     if (err) {
@@ -15,7 +20,6 @@ server.register(Inert, (err) => {
         method: 'GET',
         path: '/',
         handler: function (request, reply) {
-            console.log("1");
             reply.file(__dirname + '/views/about.html');
         }
     });
@@ -31,6 +35,24 @@ server.register(Inert, (err) => {
     });
 });
 
-server.start(() => {
-    console.log('Server running at:', server.info.uri);
+
+var options = {
+    opsInterval: 1000,
+    reporters: [{
+        reporter: GoodConsole,
+        events: { log: '*', response: '*' }
+    }]
+};
+
+server.register({
+    register: Good,
+    options: options
+}, function (err) {
+    if (err) {
+        console.error(err);
+    } else {
+        server.start(() => {
+            console.log('Server running at:', server.info.uri);
+        });
+    }
 });
