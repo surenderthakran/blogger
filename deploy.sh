943ca54d1485d0348aa084d14b749569c05bbb7a
surenderthakran <surender.thakran89@gmail.com>
2016-05-22 19:13:18 +0530

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "Deploying build $1 ..."
-git status
+BUILD_NUMBER=$1
+
+echo "Deploying build $BUILD_NUMBER ..."
+docker build -t blogger_image_${BUILD_NUMBER} .
+docker stop $(docker ps -a | grep blogger | awk '{print $1}')
+docker run -it -d --name blogger_container_${BUILD_NUMBER} -p 80:18660 blogger_image_${BUILD_NUMBER}
+docker rm $(docker ps -a | grep blogger | awk '{print $1}')
+docker rmi $(docker images | grep blogger | awk '{print $1}')
