82b9a225ff1de5d7bb5b3c5cebf6a32638f51315
surenderthakran <surender.thakran89@gmail.com>
2016-05-22 19:19:19 +0530

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,18 @@
 BUILD_NUMBER=$1
 
 echo "Deploying build $BUILD_NUMBER ..."
+
+echo "Building blogger docker image....."
 docker build -t blogger_image_${BUILD_NUMBER} .
+
+echo "Stopping previously running blogger containers..."
 docker stop $(docker ps -a | grep blogger | awk '{print $1}')
+
+echo "Running blogger container..."
 docker run -it -d --name blogger_container_${BUILD_NUMBER} -p 80:18660 blogger_image_${BUILD_NUMBER}
+
+echo "Stopping previously stopped blogger containers..."
 docker rm $(docker ps -a | grep blogger | awk '{print $1}')
+
+echo "Removing old blogger docker images..."
 docker rmi $(docker images | grep blogger | awk '{print $1}')
