308856198217c994e362907fbf4dfec33475186d
surenderthakran <surender.thakran89@gmail.com>
2016-05-31 19:23:05 +0530

    article url added to articlestore and used in home page

--- app/articlestore.js
+++ app/articlestore.js
@@ -4,6 +4,7 @@ module.exports = [
     {
         article_id: "s3-browser-upload-with-nodejs",
         template_data: {
+            url: "articles/tech/s3-browser-upload-with-nodejs",
             head: {
                 title: "AWS S3 Direct Upload from Browser with Node.js as Backend | Surender Thakran",
                 description: "Amazon Web Services (AWS) S3 Direct Upload from Browser with Node.js as Backend with example",
