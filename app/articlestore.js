af1770bf1ba57eb54f47a9965ba732f695261a60
surenderthakran <surender.thakran89@gmail.com>
2016-06-02 15:53:12 +0530

    new article added

--- app/articlestore.js
+++ app/articlestore.js
@@ -16,5 +16,21 @@ module.exports = [
                 date: "28th May 2016"
             }
         }
+    },
+    {
+        article_id: "enable-docker-remote-api-connect-ssh-tunnel",
+        template_data: {
+            url: "articles/tech/enable-docker-remote-api-connect-ssh-tunnel",
+            head: {
+                title: "Enable Docker Remote API on Remote Docker Host and Connect Using SSH Tunnel On Ubuntu 14.04 | Surender Thakran",
+                description: "",
+                keywords: ""
+            },
+            article: {
+                title: "Enable Docker Remote API on Remote Docker Host and Connect Using SSH Tunnel On Ubuntu 14.04",
+                datetime: "2016-06-02",
+                date: "2nd June 2016"
+            }
+        }
     }
 ];
