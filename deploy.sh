ac5f50c0fb8e1c493741f18ea954704d3a631771
surenderthakran <surender.thakran89@gmail.com>
2016-06-16 13:34:44 +0530

    deployment script updated

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,10 @@ echo "Stopping previously running blogger containers..."
 docker stop $(docker ps -a | grep blogger_container | awk '{print $1}')
 
 echo "Running blogger container..."
-docker run -it -d --name ${BLOGGER_CONTAINER_NAME} -p 80:18660 ${BLOGGER_IMAGE_NAME}
+docker run -it -d --name ${BLOGGER_CONTAINER_NAME} -p 18660:18660 ${BLOGGER_IMAGE_NAME}
 
 echo "Removing previously stopped blogger containers..."
 docker rm $(docker ps -f "status=exited" | grep blogger_container | awk '{print $1}')
 
-#TODO: Fix docker daemon error on removing referenced images and causing unstable builds in Jenkins
 echo "Removing old blogger docker images..."
 docker rmi $(docker images | grep blogger_image | grep -v ${BLOGGER_IMAGE_NAME} | awk '{print $1}')
