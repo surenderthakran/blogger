301f69421d91b028450a2174e5a6400a300d8f54
surenderthakran <surender.thakran89@gmail.com>
2017-02-19 02:04:37 +0530

    gulp lint task updated

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,13 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var stylish = require('jshint-stylish');
-var path = require('path');
 
 // creates a vulcanized main.css file for main.scss and its dependencies.
 gulp.task('sass', function () {
-    console.log("in sass");
     return gulp.src(__dirname + '/app/views/app/scss/main.scss')
         .pipe(sass.sync().on('error', sass.logError))
         .pipe(gulp.dest(__dirname + '/app/views/dist/css'));
@@ -20,17 +18,16 @@ gulp.task('sass', function () {
 
 // creates minimized main.min.css for main.css.
 gulp.task('clean-css', function() {
-    console.log("in clean-css");
     return gulp.src(__dirname + '/app/views/dist/css/main.css')
         .pipe(cleanCSS({debug: true}, function(details) {
             console.log(details.name + ': ' + details.stats.originalSize);
             console.log(details.name + ': ' + details.stats.minifiedSize);
         }))
-        .pipe(rename("main.min.css"))
+        .pipe(rename('main.min.css'))
         .pipe(gulp.dest(__dirname + '/app/views/dist/css'));
 });
 
-gulp.task('lint-app', function() {
+gulp.task('js-lint', function() {
   return gulp.src([
     './app/**/*.js',
     '!./app/views/dist{,/**}'])
@@ -51,13 +48,10 @@ gulp.task('js-pack', function() {
 
 // watches various frontend resources and runs tasks when saved.
 gulp.task('watch', function () {
-    console.log("in watch");
     // watches all .scss files and runs sass and clean-css tasks when saved.
     gulp.watch(__dirname + '/app/views/app/scss/**/*.scss', ['sass', 'clean-css']);
     // watches all frontend .js files and runs js-pack task when saved.
     gulp.watch(__dirname + '/app/views/app/js/**/*.js', ['js-pack']);
-    // watches all .js files and runs lint-app task when saved.
-    gulp.watch(__dirname + '/app/**/*.js', ['lint-app']);
+    // watches all .js files and runs js-lint task when saved.
+    gulp.watch(__dirname + '/app/**/*.js', ['js-lint']);
 });
-
-gulp.task('build', ['sass']);
