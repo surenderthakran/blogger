baaf28cce3cbe93362f5c6c7fc764423abd332a6
surenderthakran <surender.thakran89@gmail.com>
2017-02-19 02:32:53 +0530

    merged css packing in a single gulp task, running js-lint for all js files and fixed lint issues in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,35 +9,31 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var stylish = require('jshint-stylish');
 
-// creates a vulcanized main.css file for main.scss and its dependencies.
-gulp.task('sass', function () {
-    return gulp.src(__dirname + '/app/views/app/scss/main.scss')
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest(__dirname + '/app/views/dist/css'));
+// compiles and compresses all .scss files.
+gulp.task('css-pack', function() {
+  return gulp.src(__dirname + '/app/views/app/scss/main.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest(__dirname + '/app/views/dist/css'))
+    .pipe(cleanCSS({debug: true}, function(details) {
+      console.log(details.name + ': ' + details.stats.originalSize);
+      console.log(details.name + ': ' + details.stats.minifiedSize);
+    }))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest(__dirname + '/app/views/dist/css'));
 });
 
-// creates minimized main.min.css for main.css.
-gulp.task('clean-css', function() {
-    return gulp.src(__dirname + '/app/views/dist/css/main.css')
-        .pipe(cleanCSS({debug: true}, function(details) {
-            console.log(details.name + ': ' + details.stats.originalSize);
-            console.log(details.name + ': ' + details.stats.minifiedSize);
-        }))
-        .pipe(rename('main.min.css'))
-        .pipe(gulp.dest(__dirname + '/app/views/dist/css'));
-});
-
+// tests javascript for lint errors and reports to console.
 gulp.task('js-lint', function() {
   return gulp.src([
-    './app/**/*.js',
-    '!./app/views/dist{,/**}'])
+    './**/*.js',
+    '!./app/views/dist{,/**}',
+    '!./node_modules{,/**}'])
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
 
-// combines and compresses all .js files
+// combines and compresses all .js files.
 gulp.task('js-pack', function() {
-  console.log('in js-pack');
   return gulp.src(__dirname + '/app/views/app/js/*.js')
     .pipe(concat('main.js'))
     .pipe(gulp.dest(__dirname + '/app/views/dist/js'))
@@ -48,10 +44,10 @@ gulp.task('js-pack', function() {
 
 // watches various frontend resources and runs tasks when saved.
 gulp.task('watch', function () {
-    // watches all .scss files and runs sass and clean-css tasks when saved.
-    gulp.watch(__dirname + '/app/views/app/scss/**/*.scss', ['sass', 'clean-css']);
-    // watches all frontend .js files and runs js-pack task when saved.
-    gulp.watch(__dirname + '/app/views/app/js/**/*.js', ['js-pack']);
-    // watches all .js files and runs js-lint task when saved.
-    gulp.watch(__dirname + '/app/**/*.js', ['js-lint']);
+  // watches all .scss files and runs css-pack task when saved.
+  gulp.watch(__dirname + '/app/views/app/scss/**/*.scss', ['css-pack']);
+  // watches all frontend .js files and runs js-pack task when saved.
+  gulp.watch(__dirname + '/app/views/app/js/**/*.js', ['js-pack']);
+  // watches all .js files and runs js-lint task when saved.
+  gulp.watch(__dirname + '/app/**/*.js', ['js-lint']);
 });
