3ed27082e7e3f354de4026b4849ce7ecd667e488
surenderthakran <surender.thakran89@gmail.com>
2017-02-25 00:46:44 +0530

    table of content's internal hyperlinks changed from numbers to text

--- app/views/dist/js/main.js
+++ app/views/dist/js/main.js
@@ -6,7 +6,7 @@
     if (Utils.IsArticlePage()) {
       var toc = new TableOfContent();
       toc.GenerateIndexObject();
-      toc.CreateIndex();
+      toc.CreateIndexList();
     }
   });
 })();
@@ -17,9 +17,18 @@ const headers = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'];
 
 function IndexElement(element) {
   this.element = element;
+  this.text = '';
+  this.href = '';
   // TODO(surenderthakran): update to store children for multilevel index list.
   this.children = [];
 }
+
+IndexElement.prototype.createItemData = function() {
+  this.text = this.element.textContent;
+  var normalized = this.text.toLowerCase().replace(/\s+/g, '-')
+      .replace(/[^a-z0-9+-]+/gi, '');
+  this.href = normalized.split('-', 4).join('-');
+};
 
 function TableOfContent() {
   this.index = [];
@@ -31,21 +40,22 @@ TableOfContent.prototype.GenerateIndexObject = function() {
   for (var i = 0, len = childs.length; i < len; i++) {
     if (headers.indexOf(childs[i].tagName) !== -1) {
       var indexElement = new IndexElement(childs[i]);
+      indexElement.createItemData();
       this.index.push(indexElement);
     }
   }
 };
 
-TableOfContent.prototype.CreateIndex = function() {
+TableOfContent.prototype.CreateIndexList = function() {
   var indexList = document.createElement('ul');
   indexList.className = 'article-index';
   for (var i = 0, len = this.index.length; i < len; i++) {
-    this.index[i].element.id = i;
+    this.index[i].element.id = this.index[i].href;
     var listItem = document.createElement('li');
     var itemAnchor = document.createElement('a');
-    var itemText = document.createTextNode(this.index[i].element.textContent);
+    var itemText = document.createTextNode(this.index[i].text);
     itemAnchor.appendChild(itemText);
-    itemAnchor.href = '#' + i;
+    itemAnchor.href = '#' + this.index[i].href;
     listItem.appendChild(itemAnchor);
     indexList.appendChild(listItem);
   }
