0077035c1ac8ed4cfdfb5030a34a246f0f4f61ad
surenderthakran <surender.thakran89@gmail.com>
2017-05-30 18:54:40 +0530

    New article on dockerizing Go started

--- app/articlestore.js
+++ app/articlestore.js
@@ -2,18 +2,32 @@
 
 module.exports = [
   {
+    articleId: 'dockerized-development-and-production-environment-golang',
+    url: 'articles/tech/dockerized-development-and-production-environment-golang',
+    head: {
+      title: 'Dockerized Development and Production Environment For Go (GoLang) | Surender Thakran',
+      description: 'Dockerized Development and Production Environment For Go (GoLang)',
+      keywords: 'docker,go,golang,dockercompose,docker-compose,compose,testing',
+    },
+    article: {
+      title: 'Dockerized Development and Production Environment For Go (GoLang)',
+      datetime: '2017-05-30',
+      date: '30th May 2017',
+    },
+  },
+  {
     articleId: 'enable-docker-remote-api-connect-ssh-tunnel',
     url: 'articles/tech/enable-docker-remote-api-connect-ssh-tunnel',
     head: {
       title: 'Enable Docker Remote API on Remote Docker Host and Connect Using SSH Tunnel On Ubuntu 14.04 | Surender Thakran',
       description: 'Enable Docker Remote API on Remote Docker Host and Connect Using SSH Tunnel On Ubuntu 14.04',
-      keywords: 'docker,ssh,ubuntu,14.04,trusty'
+      keywords: 'docker,ssh,ubuntu,14.04,trusty',
     },
     article: {
       title: 'Enable Docker Remote API on Remote Docker Host and Connect Using SSH Tunnel On Ubuntu 14.04',
       datetime: '2016-06-02',
-      date: '2nd June 2016'
-    }
+      date: '2nd June 2016',
+    },
   },
   {
     articleId: 's3-browser-upload-with-nodejs',
@@ -21,12 +35,12 @@ module.exports = [
     head: {
       title: 'AWS S3 Direct Upload from Browser with Node.js as Backend | Surender Thakran',
       description: 'Amazon Web Services (AWS) S3 Direct Upload from Browser with Node.js as Backend with example',
-      keywords: 'aws,s3,nodejs,node.js,express.js,expressjs,ajax,xhr,javascript,crypto,html,form,formdata'
+      keywords: 'aws,s3,nodejs,node.js,express.js,expressjs,ajax,xhr,javascript,crypto,html,form,formdata',
     },
     article: {
       title: 'AWS S3 Direct Upload from Browser with Node.js as Backend',
       datetime: '2016-05-28',
-      date: '28th May 2016'
-    }
-  }
+      date: '28th May 2016',
+    },
+  },
 ];
