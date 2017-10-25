6f042fce45ff718076cce78b0904781d3ca66a01
surenderthakran <surender.thakran89@gmail.com>
2017-10-25 13:25:34 +0530

    updated article about dockerizing Go

--- app/articlestore.js
+++ app/articlestore.js
@@ -13,6 +13,8 @@ module.exports = [
       title: 'Dockerized Development and Production Environment For Go (GoLang)',
       datetime: '2017-05-30',
       date: '30th May 2017',
+      update_datetime: '2017-10-25',
+      update_date: '25th Oct 2017',
     },
   },
   {
