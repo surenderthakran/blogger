3b7a6853200c45db570d6f82bb6ac9b335ee1e68
surenderthakran <surender.thakran89@gmail.com>
2018-08-13 17:31:05 +0530

    v2: Updated renderer to register partials automatically.

--- v2/app/renderer/index.js
+++ v2/app/renderer/index.js
@@ -1,12 +1,37 @@
 'use strict';
 
 const fs = require('fs');
+const glob = require('glob');
 const mustache = require('mustache');
+const path = require('path');
+
 const articleStore = require(__root + '/config/articlestore');
+
+const rendererConfig = {
+  partials: {}
+};
 
 const externals = {};
 
+const registerPartials = () => {
+  let options = {
+    nodir: true,
+    strict: true,
+    cwd: process.cwd(),
+  };
+  const files = glob.sync(__root + '/views/templates/partials/*.html', options);
+
+  files.forEach((filePath) => {
+    console.log(filePath);
+    let fileName = path.basename(filePath);
+    let partialName = fileName.substring(0, fileName.indexOf('.'));
+
+    rendererConfig.partials[partialName] = fs.readFileSync(filePath, 'utf8');
+  });
+};
+
 externals.render = () => {
+  registerPartials();
   const index = fs.readFileSync(__root + '/views/templates/index.html', 'utf8');
   const html = mustache.render(index, {
     head: {
@@ -15,10 +40,7 @@ externals.render = () => {
       keywords: 'web,css3,html5',
     },
     articles: articleStore,
-  }, {
-    head_content: fs.readFileSync(__root + '/views/templates/partials/head_content.html', 'utf8'),
-    header: fs.readFileSync(__root + '/views/templates/partials/header.html', 'utf8')
-  });
+  }, rendererConfig.partials);
 
   fs.writeFile(__root + '/public/index.html', html, function (err) {
     if (err) throw err;
