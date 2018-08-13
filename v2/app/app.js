f915992fad1d24117247e6737c4c7539f3eee2f5
surenderthakran <surender.thakran89@gmail.com>
2018-08-13 15:52:33 +0530

    v2: Added middleware to serve static files from public/ directory.

--- v2/app/app.js
+++ v2/app/app.js
@@ -10,6 +10,8 @@ renderer.render();
 
 const app = express();
 
+app.use(express.static(__root + '/public'));
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(18660, () => console.log('Example app listening on port 18660!'));
