3838d419f4ddb89c3c7c124b0078888c317eed26
surenderthakran <surender.thakran89@gmail.com>
2018-08-14 15:27:49 +0530

    v2: Updated gulpfile to convert less files to css.

--- v2/gulpfile.js
+++ v2/gulpfile.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const less = require('gulp-less');
+var rename = require('gulp-rename');
 
 gulp.task('eslint', function() {
   return gulp.src([
@@ -13,6 +15,14 @@ gulp.task('eslint', function() {
     .pipe(eslint.failAfterError());
 });
 
+gulp.task('less', function () {
+  return gulp.src(__dirname + '/app/views/less/**/*.less')
+    .pipe(less())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('./app/public/css'));
+});
+
 gulp.task('watch', function() {
   gulp.watch(__dirname + '/app/**/*.js', ['eslint']);
+  gulp.watch(__dirname + '/app/views/less/**/*.less', ['less']);
 });
