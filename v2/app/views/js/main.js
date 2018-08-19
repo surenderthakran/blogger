42568d0d0fcf98e9595003c5b2d7a29e7a51010b
surenderthakran <surender.thakran89@gmail.com>
2018-08-19 22:02:35 +0530

    v2: Updated to minimize header on scroll.

--- v2/app/views/js/main.js
+++ v2/app/views/js/main.js
@@ -1,7 +1,19 @@
 'use strict';
 
 (() => {
-  document.addEventListener('DOMContentLoaded', () => {
-    console.log('here');
-  });
+  window.addEventListener('scroll', changeHeaderOnScroll);
+
+  function changeHeaderOnScroll() {
+    const owner = document.getElementById('header-image');
+    const scroll = window.pageYOffset || document.documentElement.scrollTop;
+    const threshold = 100;
+
+    owner.style.marginTop = -scroll + 'px';
+
+    if (scroll > threshold) {
+      owner.style.display = 'none';
+    } else {
+      owner.style.display = 'block';
+    }
+  };
 })();
